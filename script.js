a46f38d7d6a850af8bd6a9871ccf99f9ed4832e1
MrY8s <51115147+MrY8s@users.noreply.github.com>
2019-05-28 12:04:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// set variables
 
 api_key = "2a8ba1d2202e70e0b596c1362da04aba"
 thumbnail_size = "s" // small = s, medium = m, large = l
 currentPage = 1
-lastPage = 100
+max_tags = 3
+
 searchstring = ""
+
 
 
 function getPhotos(searchstring, pagenumber) {
 
-// run JSON query to get list of photos we are going to display
+// run JSON query to get list of photos we are going to display (then get their details)
 cxString = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+api_key+"&text="+searchstring+"&page="+pagenumber+"&per_page=4&safe_search=1&format=json&jsoncallback=?"
 
 $.getJSON(cxString, function (data) {
@@ -26,6 +29,7 @@ $.getJSON(cxString, function (data) {
             var secret = data2.photo.secret
             var title = data2.photo.title._content
           
+          // ensure title isn't too large
             if (title.length > 20) {
 
                title = title.substring(1, 20) + "...";
@@ -36,6 +40,7 @@ $.getJSON(cxString, function (data) {
             
 
             var description = data2.photo.description._content
+            
             // need to limit description so it's not an essay!
 
             if (description.length > 100) {
@@ -57,8 +62,8 @@ $.getJSON(cxString, function (data) {
           $.each(data_tags.photo.tags.tag, function (i, set) {
           var tag_content = set._content
         
-
-          if (count < 3) {
+          //append up to 3 tags (otherwise it becomes too big)
+          if (count < max_tags) {
           list_tags += (tag_content+", ")};
           count += 1;
           });
@@ -66,7 +71,8 @@ $.getJSON(cxString, function (data) {
           // tidy up tags string - remove extraaneous comma from end!
           last_comma = list_tags.lastIndexOf(", ");
           list_tags = list_tags.slice(0, last_comma);
-
+          
+          // build the html output to show the images and details
           $(list).append("<td><a href='https://flickr.com/"+nsid+"/"+id+"' target='new'><center><img src='https://farm" + farm + ".staticflickr.com/" + server + "/" +id+ "_" +secret + "_"+thumbnail_size+".jpg'></a><h3><a href='https://flickr.com/"+nsid+"/"+id+"' target='new'>"+title+"</a> by <a href=https://flickr.com/"+nsid+" target='new'>" +author+"</a></h3><h3>Description:"+description+"</h3><h3>Tags:"+list_tags+"</h3></td>")
 });
  
@@ -81,22 +87,31 @@ $.getJSON(cxString, function (data) {
 };
 
 function doSearch() {
-// 1: clear results
+// 1: clear results from web page
 $("#flickr_photos").empty();
+// 2: search for and load new image results
 searchstring = document.getElementById("searchBox").value;
-getPhotos(searchstring, 1);
-getPhotos(searchstring, 2);
-getPhotos(searchstring, 3);
+retrieve3rows();
 };
 
 function checkbottomofpage() {
+  
+  // if user scrolls to bottom of page
+  // then more images are loaded and displayed
+
   if($(window).scrollTop() + $(window).height() == $(document).height() ) {
 
-    currentPage += 1;
-    getPhotos(searchstring, currentPage);
-    currentPage += 1;
-    getPhotos(searchstring, currentPage);
-    currentPage += 1;
-    getPhotos(searchstring, currentPage);
+    retrieve3rows();
   };
 };
+
+function retrieve3rows(){
+  // 3 rows ideal - just extends past 1 page - so it will autoload content when you scroll to bottom
+    getPhotos(searchstring, currentPage);
+    currentPage += 1;
+    getPhotos(searchstring, currentPage);
+    currentPage += 1;
+    getPhotos(searchstring, currentPage);
+    currentPage += 1;
+  
+}
